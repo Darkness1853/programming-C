8dc7cc2effc46ee3072254a00200b2f22c0a1885
Darkness1853 <petya.nguen@bk.ru>
2025-05-20 01:35:25 +0300

    inverse_matrix

--- Lab/1 курс 2 семестр/1 курс 2 семестр/РГЗ/inverse_matrix/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/Users/~Darr_Kness~/programming/ClassWork/inverse_matrix
-# Build directory: /mnt/c/Users/~Darr_Kness~/programming/ClassWork/inverse_matrix/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(matrix_test "/mnt/c/Users/~Darr_Kness~/programming/ClassWork/inverse_matrix/build/test_matrix")
-set_tests_properties(matrix_test PROPERTIES  _BACKTRACE_TRIPLES "/mnt/c/Users/~Darr_Kness~/programming/ClassWork/inverse_matrix/CMakeLists.txt;19;add_test;/mnt/c/Users/~Darr_Kness~/programming/ClassWork/inverse_matrix/CMakeLists.txt;0;")
