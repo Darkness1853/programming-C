662b5716149049072b48d385783d0ab60fff7784
Darkness1853 <petya.nguen@bk.ru>
2025-05-20 09:14:37 +0300

    del

--- Lab/1 курс 2 семестр/1 курс 2 семестр/РГЗ/inverse_matrix/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/Users/~Darr_Kness~/programming/ClassWork/inverse_matrix
-# Build directory: /mnt/c/Users/~Darr_Kness~/programming/ClassWork/inverse_matrix/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("test")
--- Lab/1 курс 2 семестр/1 курс 2 семестр/РГЗ/inverse_matrix/build/test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/Users/~Darr_Kness~/programming/ClassWork/inverse_matrix/test
-# Build directory: /mnt/c/Users/~Darr_Kness~/programming/ClassWork/inverse_matrix/build/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(matrix_tests "/mnt/c/Users/~Darr_Kness~/programming/ClassWork/inverse_matrix/build/test/test_matrix")
-set_tests_properties(matrix_tests PROPERTIES  _BACKTRACE_TRIPLES "/mnt/c/Users/~Darr_Kness~/programming/ClassWork/inverse_matrix/test/CMakeLists.txt;21;add_test;/mnt/c/Users/~Darr_Kness~/programming/ClassWork/inverse_matrix/test/CMakeLists.txt;0;")
